d367021c6fbce1ecdc1fbbb126108d7e0ef2f0e0
cheddarChoi <daeun.choi@kaist.ac.kr>
2023-08-16 14:08:02 +0900

    Add server features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_cors import CORS
 from datetime import datetime
 import os, base64, random, time, json
@@ -28,8 +28,8 @@ ovsegPredictor = None
 app = Flask(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-@app.route("/setup", methods=['GET'])
-def setup():
+@app.route("/setupLarge", methods=['GET'])
+def setup_2():
     global blipprocessor, blipmodel, ovsegPredictor
     
     # Setup BLIP model
@@ -55,11 +55,23 @@ def setup():
     ovsegPredictor = VisualizationDemo(cfg)
     return 'setup done'
 
+@app.route("/setup", methods=['GET'])
+def setup():
+    global blipprocessor, blipmodel
+    
+    # Setup BLIP model
+    blipprocessor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+    blipmodel = Blip2ForConditionalGeneration.from_pretrained(
+        "Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16
+    ).to(DEVICE)
+    return 'setup done'
+
+
 @app.route("/", methods=['GET'])
 def test():
     return 'hello, world!'
 
-@app.route('/imageElement/sendImage', methods=['POST'])
+@app.route('/sendImage', methods=['POST'])
 # @cross_origin()
 def register_new_image():
     data = request.get_json()
@@ -196,17 +208,116 @@ def register_new_image():
 
     return {'filename': filename, 'keywords': keywords}, 200
 
-@app.route('/imageElement/sendElement', methods=['POST'])
-def element_merge():
-    # if request.method == 'OPTIONS':
-    #     return '', 200
+@app.route('/getElement', methods=['POST'])
+def extract_element_from_image():
+    data = request.get_json()
+    image = data.get('image')
+    if not image:
+        return {'message': 'No file received'}, 400
+
+    file_content = image.split(';base64,')[1]
+    file_extension = image.split(';')[0].split('/')[1]
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = f"{timestamp}.{file_extension}"
+
+    with open("uploaded/"+filename, 'wb') as f:
+        f.write(base64.b64decode(file_content))
+        
+    # Load image
+    print("Loading image")
+    IMAGE_PATH = "uploaded/"+filename
+    image_bgr = cv2.imread(IMAGE_PATH)
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+
+    # Get whole image description
+    print("Getting whole image description")
+    inputs = blipprocessor(images=image_rgb, return_tensors="pt").to(DEVICE, torch.float16)
+    generated_ids = blipmodel.generate(**inputs)
+    whole_image_description = blipprocessor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+    segmented_descriptions = whole_image_description + "\n"
     
+    # Get segmented image descriptions
+    print("Getting segmented image descriptions")
+    image_segments = []
+    for i in range(3):
+        for j in range(3):
+            w = int(image_rgb.shape[0] / 2)
+            h = int(image_rgb.shape[1] / 2)
+            x = int(image_rgb.shape[0] / 4 * i)
+            y = int(image_rgb.shape[1] / 4 * j)
+            image_segments.append(image_rgb[x:x+w, y:y+h])
+    for seg in image_segments:
+        inputs = blipprocessor(images=seg, return_tensors="pt").to(DEVICE, torch.float16)
+        generated_ids = blipmodel.generate(**inputs)
+        generated_text = blipprocessor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+        segmented_descriptions += generated_text + "\n"
+    
+    def get_gpt_response(description):
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {
+                "role": "system",
+                "content": "You will be provided with multiple sentences to describe an illustration. Your task is to extract a list of objects, actions, and concepts.\n\nObjects are words about the specific physical objects found in the sentences. DO NOT INCLUDE BACKGROUND OR HIGH-LEVEL WORDS SUCH AS \"ITEM\" OR \"OBJECT\".\n\nConcepts are words not found in the given text, but can potentially convey the overall mood or topic of the illustration."
+                },
+                {
+                "role": "user",
+                "content": "a card with chinese writing with colorful objects on it\na red and orange background with a blank paper, chinese, pencils, stationery and more\nan image of a classroom scene with various supplies"
+                },
+                {
+                "role": "assistant",
+                "content": "Objects: card, Chinese writing, blank paper, pencils, stationery, classroom\n\nActions: \n\nConcepts: education, learning, multiculturalism."
+                },
+                {
+                "role": "user",
+                "content": description
+                },
+            ],
+            temperature=0.5,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        res = response.choices[0].message.content
+        objects = res.split('\n\n')[0].split(': ')[1].strip('.').split(', ')
+        actions = res.split('\n\n')[1].split(': ')[1].strip('.').split(', ')
+        concepts = res.split('\n\n')[2].split(': ')[1].strip('.').split(', ')
+        
+        return objects, actions, concepts
+    
+    print("Getting GPT response")
+    objects, _, _ = get_gpt_response(whole_image_description)
+    _, actions, concepts = get_gpt_response(segmented_descriptions)
+    
+    keywords = []
+    
+    for obj in objects:
+        keywords.append({
+            "type": "object", 
+            "keyword": obj, 
+            "position": None
+        })
+    
+    for act in actions:
+        keywords.append({
+            "type": "action", 
+            "keyword": act, 
+            "position": None
+        })
+    for con in concepts:
+        keywords.append({
+            "type": "concept", 
+            "keyword": con, 
+            "position": None
+        })
+
+    return {'filename': filename, 'keywords': keywords}, 200
+
+@app.route('/getDescriptions', methods=['POST'])
+def generate_descriptions_from_elements():
     data = request.get_json()
     elements = data.get('elements')['elements']
-    
-    # f = open("dummydata.json", "r")
-    # elements = json.loads(f.read())['elements']
-    # f.close()
     
     objects = []
     actions = []
@@ -228,7 +339,7 @@ def element_merge():
             messages=[
                 {
                 "role": "system",
-                "content": "The user wants to draw an illustration, with the assistance of you. You will be provided with multiple keywords users wanted to include in their illustrations. There are three types of keywords: object, action, and concepts.\n\nObjects are words about the specific physical objects that the user wants to include in their illustration.\n\nAction is about the action that the character or animal in the illustration performs.\n\nConcepts are words not found in the illustration directly, but can potentially convey the overall mood or topic of the illustration.\n\nYour task is to generate three descriptions of the illustration that the user can draw based on the given keywords. Each description should be in two concise lines, one for the background, and one for the scene."
+                "content": "The user wants to draw an illustration, with the assistance of you. You will be provided with multiple keywords users wanted to include in their illustrations. There are three types of keywords: object, action, and concepts.\n\nObjects are words about the specific physical objects that the user wants to include in their illustration.\n\nAction is about the action that the character or animal in the illustration performs.\n\nConcepts are words not found in the illustration directly, but can potentially convey the overall mood or topic of the illustration.\n\nYour task is to generate three descriptions of the illustration that the user can draw based on the given keywords. Each description should include two things: \"Background\" and \"Objects\".\n\n\"Objects\" is a list of the objects depicted in the illustrations, and the short description of them. The objects should be one of the given objects, or closely related to them. The given action and concept should be also considered for generating the detail of the object.\n\nThe background is a one-line concise sentence describing the background of the illustration. It should be well aligned with the objects depicted and the given concepts."
                 },
                 {
                 "role": "user",
@@ -236,7 +347,7 @@ def element_merge():
                 },
                 {
                 "role": "assistant",
-                "content": "Background: A sunny park with lush green grass and colorful flowers.\nScene: A playful cat and a joyful dog chasing a bouncing ball together.\n\nBackground: A cozy living room with a warm fireplace and comfortable furniture.\nScene: A peaceful cat gracefully leaping through the air, while a playful dog jumps energetically after a bouncing ball.\n\nBackground: A serene beach with soft sand, sparkling blue waves, and seashells scattered along the shore.\nScene: A carefree cat and an exuberant dog leaping in sync, as they joyfully jump after a ball rolling on the sandy beach."
+                "content": "Objects: [(cat, a playful white cat), (dog, a joyful dog chasing a bouncing ball)]\nBackground: A sunny park with lush green grass and colorful flowers.\n\nObjects: [(cat, a cat leaping through the air), (dog, a dog jumping after a bouncing ball)]\nBackground: A cozy living room with a warm fireplace and comfortable furniture.\n\nObjects: [(dog, an exuberant dog), (cat, a carefree cat), (ball, rolling on the beach)]\nBackground: A serene beach with soft sand, sparkling blue waves, and seashells scattered along the shore."
                 },
                 {
                 "role": "user",
@@ -244,15 +355,15 @@ def element_merge():
                 },
                 {
                 "role": "assistant",
-                "content": "Background: A softly lit, intriguing living room, with old mystery books scattered across wooden shelves. \nScene: On the comfy sofa, a skeptical person relaxes, gazing at the ghostly silhouette of an air balloon projected onto the wall. \n\nBackground: A spacious, cozy attic with vintage decorations and star-themed wallpaper.\nScene: A person, donned in relaxed loungewear, sits skeptically on a plush sofa, observing a projected air balloon mysteriously floating on the ceiling.\n\nBackground: A circular room adorned with astronomical murals and whimsical twinkling lights for a starry effect. \nScene: A dubious figure lounges restfully on an antique sofa, eyes following the floating journey of a projected air balloon around the room."
+                "content": "Objects: [(sofa, a sofa with a person lounging on it), (air balloon, mini air balloon hovering above the floor), (projector, a projector displaying a sci-fi movie)]\nBackground: A loft-style living room filled with modern, minimalistic furniture and large windows.\n\nObjects: [(sofa, an inviting, plush sofa), (projector, an old projector screening a noir film), (air balloon, a model air balloon)]\nBackground: A dimly lit, cozy media room with posters of classic movies on the walls.\n\nObjects: [(sofa, a vintage leather sofa with a person sitting in contemplation), (projector, a projector casting light and shadow across the room), (air balloon, an air balloon-shaped lamp hanging from the ceiling)]\nBackground: A distinctive, retro-themed lounge filled with a collection of antique items."
                 },
                 {
                 "role": "user",
-                "content": "Object: bar graph, cell phone, grad student\nAction: researching\nConcept: sad"
+                "content": "Object: bar graph, cell phone, grad student\nAction: reading, standing\nConcept: gloomy, blue, research"
                 },
                 {
                 "role": "assistant",
-                "content": "Background: A cluttered grad school dorm room filled with textbooks, sticky notes, and mug-full of half-drunk coffee. \nScene: A tired grad student sits slumped over, sadly researching on their cell phone, with a bar graph displayed despairingly on their cracked screen.\n\nBackground: A dimly lit, silent library corner with towering bookshelves all around.\nScene: A disheartened grad student is hunched over a bar graph, meticulously researching data on their cell phone with an air of melancholy.\n\nBackground: A quiet study room, where the fading sunset casts long, forlorn shadows. \nScene: A downcast grad student deeply engrossed in researching, sadly staring at a bar graph displayed on their cellphone screen."
+                "content": "Objects: [(cell phone, a grad student standing holding a phone), (bar graph, a bar graph on the screen showing statistics)]\nBackground: A dimly lit room barely illuminating a cluttered study desk filled with research papers and stacks of books.\n\nObjects: [(grad student, a grad student in a blue sweater, standing and reading statistical data on a cell phone), (bar graph, a complex bar graph depicts recent research results)]\nBackground: A library corner with towering bookshelves and a gloomy atmosphere, scattered with open books and coffee cups.\n\nObjects: [(bar graph, a large digital bar graph on the phone screen), (grad student, a grad student looking visibly stressed)]\nBackground: A lab filled with computers emitting a soft glow, research papers scattered everywhere."
                 },
                 {
                 "role": "user",
@@ -267,13 +378,26 @@ def element_merge():
         )
         res = []
         for r in response.choices[0].message.content.split('\n\n'):
-            background, scene = r.split('\n')
-            background = background.strip(' ').split(': ')[1]
-            scene = scene.strip(' ').split(': ')[1]
+            print(r)
+            objects = []
+            background = ""
+            for line in r.split('\n'):
+                if line.startswith('Objects'):
+                    tmp = line.split(': ')[1].strip('[]').split('), ')
+                    for (obj, i) in zip(tmp, range(len(tmp))):
+                        obj = obj.strip('()').split(', ')
+                        objects.append({
+                            "object": obj[0],
+                            "detail": obj[1],
+                            "id": i+1
+                        })
+                elif line.startswith('Background'):
+                    background = line.split(': ')[1]
             res.append({
-                "background": background,
-                "scene": scene
+                "objects": objects,
+                "background": background
             })
+        
         return res
     
     print("Getting GPT response")
@@ -281,6 +405,14 @@ def element_merge():
 
     return {"descriptions": res}, 200
 
+@app.route('/getImages', methods=['POST'])
+def generate_recombined_images():
+    data = request.get_json()
+    data = data.get('data')
+    print(data)
+    
+    image_path = "generated/" + "test.png"
+    return send_file(image_path, mimetype='image/png')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=52000, debug=True)
